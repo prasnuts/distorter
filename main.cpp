e236d2fe6b8afd742782b21054e585ce1025d945
Prasad Samarakoon <prasnuts@gmail.com>
2020-03-24 20:35:23 +0100

    [dev] use image provider and distorter

--- main.cpp
+++ main.cpp
@@ -4,9 +4,23 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
+#include "util/Distorter.hpp"
+#include "util/ImageProvider.hpp"
+
 int main(int argc, char **argv) {
+    qRegisterMetaType< cv::Mat >("cv::Mat");
+
     QApplication app(argc, argv);
+
+    Distorter distorter{};
+    ImageProvider * imageProvider = new ImageProvider{};
+
+    QObject::connect(&distorter, &Distorter::chessboardGenerated, imageProvider, &ImageProvider::onChessboardGenerated);
+
     QQmlApplicationEngine engine;
+    engine.addImageProvider("imageProvider", imageProvider);
+    engine.rootContext()->setContextProperty("distorter", &distorter);
+
     engine.addImportPath( "qrc://ui/." );
     engine.load(QUrl(QLatin1String("qrc:/ui/main.qml")));
     return QApplication::exec();
